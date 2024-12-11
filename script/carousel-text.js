54d82e88f7d3c1382e9fe1f13ec2ecd8d43bd6be
ROYWOODEN <ronald.brooks2006@gmail.com>
2024-12-11 10:00:25 +0300

    текст в карусели адаптация

--- script/carousel-text.js
+++ script/carousel-text.js
@@ -8,3 +8,19 @@ function toggleText(button) {
       button.textContent = "Подробнее"; // Вернуть текст кнопки
     }
   }
+
+
+
+  // Логика для кнопки "Подробнее"
+function toggleText(button) {
+  const hiddenText = button.nextElementSibling; // Найти текст рядом с кнопкой
+  const isVisible = hiddenText.style.display === 'block'; // Проверить текущее состояние
+  
+  if (isVisible) {
+    hiddenText.style.display = 'none'; // Скрыть текст
+    button.textContent = 'Подробнее'; // Изменить текст кнопки
+  } else {
+    hiddenText.style.display = 'block'; // Показать текст
+    button.textContent = 'Скрыть'; // Изменить текст кнопки
+  }
+}
